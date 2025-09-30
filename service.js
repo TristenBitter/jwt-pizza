f6c3c1ed4f2ee312fbf290fa7c32d841286806c9
TristenBitter <123581497+TristenBitter@users.noreply.github.com>
2025-09-30 15:09:50 -0600

    Update service.js

--- service.js
+++ service.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 app.use(express.json());
@@ -24,4 +24,4 @@ app.post('/cities', (req, res) => {
   }
 });
 
-module.exports = app;
+export default app;
