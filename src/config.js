6db35f1ca89226801fbccbbe8969772e45ae0473
TristenBitter <123581497+TristenBitter@users.noreply.github.com>
2025-09-30 21:15:56 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,19 +1,17 @@
-- name: Write config file
-  run: |
-    echo "module.exports = {
-      jwtSecret: '${{ secrets.JWT_SECRET }}',
-      db: {
-        connection: {
-          host: '127.0.0.1',
-          user: 'root',
-          password: 'tempdbpassword',
-          database: 'pizza',
-          connectTimeout: 60000,
-        },
-        listPerPage: 10,
-      },
-      factory: {
-        url: 'https://pizza-factory.cs329.click',
-        apiKey: '${{ secrets.FACTORY_API_KEY }}',
-      },
-    };" > src/config.js
+module.exports = {
+  jwtSecret: 'your-jwt-secret',
+  db: {
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'tempdbpassword',
+      database: 'pizza',
+      connectTimeout: 60000,
+    },
+    listPerPage: 10,
+  },
+  factory: {
+    url: 'https://pizza-factory.cs329.click',
+    apiKey: 'your-factory-api-key',
+  },
+};
